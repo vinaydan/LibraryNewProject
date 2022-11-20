c1dc22cb6f0f71596c7fe330bd40ea86edb8eb60
Vinay <vinay95d.vd@gmail.com>
2022-11-20 18:35:35 -0500

    Initial commit

---
+++ src/main/java/com/example/librarynewproject/LibraryNewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarynewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryNewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryNewProjectApplication.class, args);
+    }
+
+}
